3f068f9457f8e72731d8be5d91962d385336d3a4
clcummin <christopher.l.cummings@hotmail.com>
2025-07-18 17:00:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// Lock scroll on intro page
+document.documentElement.style.overflow = "hidden";
+document.body.style.overflow = "hidden";
+document.body.style.height = "100vh";
+
+// Fade in the card after 1 second
 window.addEventListener('DOMContentLoaded', function() {
   setTimeout(function() {
     document.getElementById('introCard').classList.add('visible');
-  }, 1000); // 1 second = 1000ms
+  }, 1000);
 });
